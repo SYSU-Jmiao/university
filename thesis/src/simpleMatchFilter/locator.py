d0e140bf7cc9be6d805b742f778fe17642edff80
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-03-25 16:59:40 +0300

    add docker-compose

--- thesis/src/simpleMatchFilter/locator.py
+++ thesis/src/simpleMatchFilter/locator.py
@@ -1,43 +1,72 @@
-#!/usr/bin/env python
+"""Find matching wave by simple correlation."""
+
+import argparse
+import glob
 import numpy as np
-import sys, getopt
 import os
-import scipy.stats as st  
 
-def result(original_file_name ,filtered_file_name):
-	#print("original:"+original_file_name,"filterd:"+filtered_file_name)
-	original_data = np.loadtxt(original_file_name,delimiter=',')
-	filtered_data = np.loadtxt(filtered_file_name,delimiter=',')
-	sizeA , null = np.shape(original_data)
-	sizeB , null = np.shape(filtered_data)
-	dataSize = sizeB
-	if sizeA > sizeB:
-		dataSize = sizeB
 
-	# print("size of sample:",dataSize)
-	y1 = np.resize(original_data[:,1],dataSize)
-	y2 = np.resize(filtered_data[:,1],dataSize)	
-	
-	score = np.corrcoef(y1,y2)
-	return score[0,1]
-	# score = st.spearmanr(y1, y2)
-	# return score[1]
+class __DataLocation:
+    def __init__(self):
+        self.original = "/dev/null"
+        self.filtered = "/dev/null"
+        self.originalfiles = []
+        self.filteredfiles = []
 
-if len(sys.argv) < 3:
-	print("not enouge argumetns")
-	sys.exit()	
 
-original_dir_name=sys.argv[1]
-filtered_dir_name=sys.argv[2]
+def __getlocationofdata():
+    parser = argparse.ArgumentParser(description='Compare tested data to\
+             syntatic waves.')
+    parser.add_argument('origin', nargs=1, help='lo\
+                        cation of the source sample files')
+    parser.add_argument('filtered', nargs=1, help='lo\
+                        cation of the filtered sample files')
+    args = parser.parse_args()
+    d = __DataLocation
+    d.origin = args.origin[0]
+    d.filtered = args.filtered[0]
+    d.originfiles = glob.glob(d.origin + "/*")
+    d.filteredfiles = glob.glob(d.filtered + "/*")
+    return d
 
-for on in os.listdir(original_dir_name):
-	match=0
-	match_name = ""
-	for fn in os.listdir(filtered_dir_name):
-	        #print (result(original_dir_name+on,filtered_dir_name+fn))
-	        p_match = result(original_dir_name+on,filtered_dir_name+fn)
-	        #if abs(p_match) > abs(match):
-	        if p_match > match:
-	        	match = p_match
-	        	match_name = fn
-	print(on,match_name)        	
+
+def __readlistoffiles(list_of_files):
+    new_dict = dict()
+    for f in list_of_files:
+        new_dict[f] = __readfile(f)
+        print("key:" + f + ", number of elements:" + str(len(new_dict[f])))
+    return new_dict
+
+
+def __readfile(dest_file):
+    print("loading:" + dest_file)
+    data = np.genfromtxt(dest_file, delimiter=',')
+    return data
+
+
+def __print_best_match(name, candidate, possibilities):
+    number_of_samples = 20000
+    max_value = 0
+    max_name = "none"
+    sig_noise = np.resize(candidate, number_of_samples)
+    for k in possibilities:
+        sig = np.resize(possibilities[k], number_of_samples)
+        corr = np.corrcoef(sig, sig_noise)
+        score = corr[0, 1]
+        if score > max_value:
+            max_name = k
+            max_value = score
+    print(os.path.basename(name) + "==" + os.path.basename(max_name))
+
+
+def main():
+    """Script starts here."""
+    locations = __getlocationofdata()
+    print(locations.__dict__)
+    originscsvs = __readlistoffiles(locations.originfiles)
+    filterdcsvs = __readlistoffiles(locations.filteredfiles)
+    for k in originscsvs:
+        __print_best_match(k, originscsvs[k], filterdcsvs)
+
+if __name__ == "__main__":
+    main()
