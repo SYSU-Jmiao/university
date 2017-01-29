cf95a3b1c581ccf1addabaab3ad9ab059e6e9daa
yonidavidson <yonidavidson@gmail.com>
2017-01-30 01:01:44 +0200

    base experiment

--- src/deepLearning/base/base-results.py
+++ src/deepLearning/base/base-results.py
@@ -1,8 +1,18 @@
 
+# coding: utf-8
+
+# In[1]:
+
+# Import all the things we need ---
+#   by setting env variables before Keras import you can set up which backend and which GPU it uses
+get_ipython().magic('matplotlib inline')
 import os,random
+#os.environ["KERAS_BACKEND"] = "theano"
 os.environ["KERAS_BACKEND"] = "tensorflow"
-os.environ["THEANO_FLAGS"]  = "device=gpu%d"%(1)
+os.environ["KERAS_FLAGS"]  = "device=gpu%d"%(0)
 import numpy as np
+import tensorflow as th
+#import tensorflow.tensor as T
 from keras.utils import np_utils
 import keras.models as models
 from keras.layers.core import Reshape,Dense,Dropout,Activation,Flatten
@@ -10,16 +20,18 @@ from keras.layers.noise import GaussianNoise
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.regularizers import *
 from keras.optimizers import adam
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import cPickle, random, sys, keras
 
 
+# In[2]:
+
+# Load the dataset ...
+#  You will need to seperately download or generate this file
 Xd = cPickle.load(open("RML2016.10a_dict.dat",'rb'))
 snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])
-X = []
+X = []  
 lbl = []
 for mod in mods:
     for snr in snrs:
@@ -28,8 +40,10 @@ for mod in mods:
 X = np.vstack(X)
 
 
+# In[3]:
+
 # Partition the data
-#  into training and test sets of the form we can train/test on
+#  into training and test sets of the form we can train/test on 
 #  while keeping SNR and Mod labels handy for each
 np.random.seed(2016)
 n_examples = X.shape[0]
@@ -46,12 +60,17 @@ Y_train = to_onehot(map(lambda x: mods.index(lbl[x][0]), train_idx))
 Y_test = to_onehot(map(lambda x: mods.index(lbl[x][0]), test_idx))
 
 
+# In[4]:
+
 in_shp = list(X_train.shape[1:])
 print X_train.shape, in_shp
 classes = mods
 
 
-# Build VT-CNN2 Neural Net model using Keras primitives --
+# In[5]:
+
+
+# Build VT-CNN2 Neural Net model using Keras primitives -- 
 #  - Reshape [N,2,128] to [N,1,2,128] on input
 #  - Pass through 2 2DConv/ReLu layers
 #  - Pass through 2 Dense layers (ReLu and Softmax)
@@ -61,10 +80,10 @@ dr = 0.5 # dropout rate (%)
 model = models.Sequential()
 model.add(Reshape([1]+in_shp, input_shape=in_shp))
 model.add(ZeroPadding2D((0, 2)))
-model.add(Convolution2D(256, 1, 3, border_mode='valid', activation="relu", name="conv1", init='glorot_uniform', dim_ordering="th"))
+model.add(Convolution2D(256, 1, 3, border_mode='valid', activation="relu", name="conv1", init='glorot_uniform'))
 model.add(Dropout(dr))
 model.add(ZeroPadding2D((0, 2)))
-model.add(Convolution2D(80, 2, 3, border_mode="valid", activation="relu", name="conv2", init='glorot_uniform', dim_ordering="th"))
+model.add(Convolution2D(80, 2, 3, border_mode="valid", activation="relu", name="conv2", init='glorot_uniform'))
 model.add(Dropout(dr))
 model.add(Flatten())
 model.add(Dense(256, activation='relu', init='he_normal', name="dense1"))
@@ -78,11 +97,12 @@ model.summary()
 
 # In[6]:
 
-# Set up some params
+# Set up some params 
 nb_epoch = 100     # number of epochs to train on
-batch_size = 500  # training batch size
-#batch_size = 1024  # training batch size
+batch_size = 1024  # training batch size
 
+
+# In[7]:
 
 # perform training ...
 #   - call the main training loop in keras for our network+dataset
@@ -98,28 +118,28 @@ history = model.fit(X_train,
         keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
         keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
     ])
-
-file = open("mylog.txt","w")
-file.write("Learning is complete")
-file.close()
-
 # we re-load the best weights once training is finished
 model.load_weights(filepath)
 
 
-# # Evaluate and Plot Model Performance
+# In[8]:
 
 # Show simple version of performance
 score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0, batch_size=batch_size)
 print score
 
-# Show loss curves
+
+# In[9]:
+
+# Show loss curves 
 plt.figure()
 plt.title('Training performance')
 plt.plot(history.epoch, history.history['loss'], label='train loss+error')
 plt.plot(history.epoch, history.history['val_loss'], label='val_error')
 plt.legend()
-plt.savefig('loss_curves.png')
+
+
+# In[10]:
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
@@ -131,8 +151,9 @@ def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, label
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    # TODO: differ the plots
-    plt.savefig(title + '.png')
+
+
+# In[11]:
 
 # Plot confusion matrix
 test_Y_hat = model.predict(X_test, batch_size=batch_size)
@@ -148,6 +169,8 @@ plot_confusion_matrix(confnorm, labels=classes)
 
 
 
+# In[12]:
+
 # Plot confusion matrix
 acc = {}
 for snr in snrs:
@@ -155,7 +178,7 @@ for snr in snrs:
     # extract classes @ SNR
     test_SNRs = map(lambda x: lbl[x][1], test_idx)
     test_X_i = X_test[np.where(np.array(test_SNRs)==snr)]
-    test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]
+    test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]    
 
     # estimate classes
     test_Y_i_hat = model.predict(test_X_i)
@@ -169,21 +192,32 @@ for snr in snrs:
         confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
     plt.figure()
     plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)"%(snr))
-
+    
     cor = np.sum(np.diag(conf))
     ncor = np.sum(conf) - cor
     print "Overall Accuracy: ", cor / (cor+ncor)
     acc[snr] = 1.0*cor/(cor+ncor)
+    
 
+
+# In[15]:
 
 # Save results to a pickle file for plotting later
 print acc
 fd = open('results_cnn2_d0.5.dat','wb')
 cPickle.dump( ("CNN2", 0.5, acc) , fd )
 
+
+# In[16]:
+
 # Plot accuracy curve
 plt.plot(snrs, map(lambda x: acc[x], snrs))
 plt.xlabel("Signal to Noise Ratio")
 plt.ylabel("Classification Accuracy")
 plt.title("CNN2 Classification Accuracy on RadioML 2016.10 Alpha")
-plt.savefig('accuracy_curve' + '.png')
+
+
+# In[ ]:
+
+
+
