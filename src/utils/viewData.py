71f7304f283d6c6f4bb0fe8c39127685304d1236
yonidavidson <yonidavidson@gmail.com>
2017-07-15 11:37:44 +0300

    Refactored

--- src/utils/viewData.py
+++ src/utils/viewData.py
@@ -1,62 +1,34 @@
-import os, random, cPickle, sys
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate, signal
 
 from dbManager import initDb
 from signalUtils import getSignalWithLabelGenerator
 from signalUtils import crossSpectrumDensity, spectogram, periodGram, cwt
 
 NUMBER_OF_SAMPLES_PER_METHOD = 6
-NUMBER_OF_ROWS = 2
-NUMBER_OF_COLUMNS = 3
+NUMBER_OF_ROWS = 3
+NUMBER_OF_COLUMNS = 2
 
 generator = getSignalWithLabelGenerator(initDb)
 
 for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD):
     sample, label = generator(x)
-    plt.subplot(3, 2, x+1)
+    plt.subplot(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, x+1)
     plt.tight_layout()
     plt.title(label)
     crossSpectrumDensity(sample)
 
 plt.show();
 
-for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
-    sample, label = generator(x)
-    plt.subplot(3, 2, x+1)
-    plt.tight_layout()
-    spectogram(sample[0])
-    plt.title( " I " + label)
-    plt.subplot(3, 2, x+2)
-    plt.tight_layout()
-    spectogram(sample[1])
-    plt.title( " Q " + label)
+for process in [spectogram, periodGram, cwt]:
+    for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
+        sample, label = generator(x)
+        plt.subplot(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, x+1)
+        plt.tight_layout()
+        process(sample[0])
+        plt.title( " I " + label)
+        plt.subplot(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, x+2)
+        plt.tight_layout()
+        process(sample[1])
+        plt.title( " Q " + label)
 
-plt.show();
-
-for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
-    sample, label = generator(x)
-    plt.subplot(3, 2, x+1)
-    plt.tight_layout()
-    periodGram(sample[0])
-    plt.title( " I " + label)
-    plt.subplot(3, 2, x+2)
-    plt.tight_layout()
-    periodGram(sample[1])
-    plt.title( " Q " + label)
-
-plt.show();
-
-for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
-    sample, label = generator(x)
-    plt.subplot(3, 2, x+1)
-    plt.tight_layout()
-    cwt(sample[0])
-    plt.title( " I " + label)
-    plt.subplot(3, 2, x+2)
-    plt.tight_layout()
-    cwt(sample[1])
-    plt.title( " Q " + label)
-
-plt.show();
+    plt.show();
