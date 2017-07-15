0718127dee5d2797db45c21e0964918c60407f90
yonidavidson <yonidavidson@gmail.com>
2017-07-15 11:32:01 +0300

    middle of refactor

--- src/utils/viewData.py
+++ src/utils/viewData.py
@@ -2,62 +2,18 @@ import os, random, cPickle, sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate, signal
+
 from dbManager import initDb
+from signalUtils import getSignalWithLabelGenerator
+from signalUtils import crossSpectrumDensity, spectogram, periodGram, cwt
 
-def getOverSampledSignal(sample, overSampleFactor):
-    sampleSize = sample.shape[0]
-    t = np.arange(0, sampleSize, 1)
-    f = interpolate.InterpolatedUnivariateSpline(t, sample)
-    tNew = np.arange(0.0, sampleSize, 1.0/overSampleFactor)
-    sNew = f(tNew)
-    return sNew
-
-def cwt(x):
-    sNew = getOverSampledSignal(x, 8)
-    width = 32
-    widths = np.arange(1, width)
-    cwtmatr = signal.cwt(sNew, signal.ricker, widths)
-    plt.imshow(cwtmatr, extent=[-1, 1, 1, width],
-               cmap='PRGn', aspect='auto', vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
-    plt.plot()
-
-def periodGram(x):
-    fs = 10e6
-    f, Pxx_den = signal.periodogram(getOverSampledSignal(x,8), fs)
-    plt.semilogy(f, Pxx_den)
-    plt.xlabel('frequency [Hz]')
-    plt.ylabel('PSD [V**2/Hz]')
-    plt.plot()
-
-def spectogram(x):
-    fs = 10e6
-    f, t, Sxx = signal.spectrogram(getOverSampledSignal(x, 8), fs)
-    plt.pcolormesh(t, f, Sxx)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.plot()
-
-def crossSpectrumDensity(sample):
-    fs = 10e6
-    x, y = sample[0], sample[1]
-    f, Pxy = signal.csd(getOverSampledSignal(x, 8),getOverSampledSignal(y, 8), fs, nperseg=1024)
-    plt.semilogy(f, np.abs(Pxy))
-    plt.xlabel('frequency [Hz]')
-    plt.ylabel('CSD [V**2/Hz]')
-    plt.plot()
-
-def getSignalWithLabelGenerator(initDb):
-    signals, labels, mods = initDb()
-    def getSignalWithLabel(x):
-        signal = signals[x]
-        label = mods[np.where(labels[x] == 1.0)[0][0]]
-        return  signal, label
-    return getSignalWithLabel
-
+NUMBER_OF_SAMPLES_PER_METHOD = 6
+NUMBER_OF_ROWS = 2
+NUMBER_OF_COLUMNS = 3
 
 generator = getSignalWithLabelGenerator(initDb)
-numberOfSamples = 6
-for x in range(0, numberOfSamples):
+
+for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD):
     sample, label = generator(x)
     plt.subplot(3, 2, x+1)
     plt.tight_layout()
@@ -66,8 +22,7 @@ for x in range(0, numberOfSamples):
 
 plt.show();
 
-numberOfSamples = 6
-for x in range(0, numberOfSamples-1, 2):
+for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
     sample, label = generator(x)
     plt.subplot(3, 2, x+1)
     plt.tight_layout()
@@ -80,8 +35,7 @@ for x in range(0, numberOfSamples-1, 2):
 
 plt.show();
 
-numberOfSamples = 6
-for x in range(0, numberOfSamples-1, 2):
+for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
     sample, label = generator(x)
     plt.subplot(3, 2, x+1)
     plt.tight_layout()
@@ -94,8 +48,7 @@ for x in range(0, numberOfSamples-1, 2):
 
 plt.show();
 
-numberOfSamples = 6
-for x in range(0, numberOfSamples-1, 2):
+for x in range(0, NUMBER_OF_SAMPLES_PER_METHOD-1, 2):
     sample, label = generator(x)
     plt.subplot(3, 2, x+1)
     plt.tight_layout()
