4ccd44e831cc50cc91e816214d3d8908b8fa31af
yonidavidson <yoni.davidson@tg-17.com>
2018-03-03 19:08:00 +0200

    create the lists

--- create_db.py
+++ create_db.py
@@ -2,6 +2,7 @@
 import sys
 import glob
 from os import path
+import random
 
 
 def get_classes_from_folders(data_dir):
@@ -11,7 +12,23 @@ def get_classes_from_folders(data_dir):
     return classes
 
 def get_data_sets(data_location):
-    return [],[]
+    # TODO: get this as variable
+    DIVISOR = 100
+    # TODO: get this as variable
+    PREDICAT = lambda x: int(x.split(".png")[0].split("_")[-1]) >= 4
+    all_files = glob.glob(path.join(data_location,'*'))
+    partial_list = filter(PREDICAT, all_files)
+    random.shuffle(partial_list,random.random)
+    train_set = partial_list[:len(partial_list)/2]
+    validation_set = partial_list[len(partial_list)/2:]
+    
+    del train_set[:len(train_set) % DIVISOR]
+    del validation_set[:len(validation_set) % DIVISOR]
+
+    print len(train_set)
+    print len(validation_set)
+
+    return train_set,validation_set
 
 def copy_data(class_name, output_dir, train_data_set, validation_data_set):
     return
@@ -32,6 +49,7 @@ if __name__ == "__main__":
     data_dir = sys.argv[1]
     output_dir = sys.argv[2]
     print "data_dir=" + data_dir
+    
     err = create_db(data_dir, output_dir)
     if err is None:
         print 'success'
