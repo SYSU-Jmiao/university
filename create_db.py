cc5a3da95967825a579616662b145cfcde91cfff
yonidavidson <yoni.davidson@tg-17.com>
2018-03-03 13:17:19 +0200

    building a data creation script

--- create_db.py
+++ create_db.py
@@ -4,11 +4,23 @@ import sys
 
 def get_classes_from_folders():
     print "hello"
+    return []
 
 
-def create_db():
+def create_db(data_dir):
     classes = get_classes_from_folders()
+    if len(classes) != 11:
+        return 'failed to collect classes'
+    return 
 
 if __name__ == "__main__":
-    argv1 = sys.argv[1]
-    print argv1
+    if len(sys.argv) < 2:
+        print 'no directorty location exiting...'
+        sys.exit(1)
+    data_dir = sys.argv[1]
+    print "data_dir=" + data_dir
+    err = create_db(data_dir)
+    if err is None:
+        print 'success'
+    else:
+        print err
