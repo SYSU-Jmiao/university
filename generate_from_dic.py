54861169c75ed6866cc8fecb8b866b45550632ac
yonidavidson <yoni.davidson@tg-17.com>
2018-04-02 18:51:10 +0300

    middle of work

--- generate_from_dic.py
+++ generate_from_dic.py
@@ -6,7 +6,14 @@ import matplotlib.pyplot as plt
 
 data = cPickle.load( open( "../data_dict.dat", "rb" ) )
 
+def create_image(index,value,label,snr):
+    print index
+    print value
+    print label
+    print snr
+
 for p in data:
     label = p[0]
     snr = str(p[1])
     print "label:"+label+ ", SNR:"+ snr
+    [create_image(index, value,label,snr) for index, value in enumerate(data[p])]
