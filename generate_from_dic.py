2bd100a5c81dbb98b865ffac240a7f586d103013
yonidavidson <yoni.davidson@tg-17.com>
2018-04-03 07:33:16 +0300

    create folders

--- generate_from_dic.py
+++ generate_from_dic.py
@@ -3,8 +3,8 @@ import cPickle
 import matplotlib     
 matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
+import os
 
-data = cPickle.load( open( "../data_dict.dat", "rb" ) )
 
 def create_image(index,value,label,snr):
     print index
@@ -12,8 +12,17 @@ def create_image(index,value,label,snr):
     print label
     print snr
 
+def create_folder(directory):    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+BASE_PATH="/tmp/data_test"
+data = cPickle.load( open( "../data_dict.dat", "rb" ) )
+
+create_folder(BASE_PATH)
 for p in data:
     label = p[0]
     snr = str(p[1])
     print "label:"+label+ ", SNR:"+ snr
+    create_folder(os.path.join(BASE_PATH,label))
     [create_image(index, value,label,snr) for index, value in enumerate(data[p])]
